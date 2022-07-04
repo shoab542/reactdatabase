5aff7e65b7215d61f38a738e2854db79160d3830
Shoab <shoab542@gmail.com>
2022-07-04 11:18:07 +0600

    Updates

---
+++ precache-manifest.d2995ec2daffb041b35fedff4cc53fb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd28dfb2190757059ed5073eabac8e8",
+    "url": "/reactdatabase/index.html"
+  },
+  {
+    "revision": "ab9b9e72d866cc2a644f",
+    "url": "/reactdatabase/static/css/main.196ebecf.chunk.css"
+  },
+  {
+    "revision": "467317d004035060c6d2",
+    "url": "/reactdatabase/static/js/2.89ebb167.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/reactdatabase/static/js/2.89ebb167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab9b9e72d866cc2a644f",
+    "url": "/reactdatabase/static/js/main.48a18b74.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/reactdatabase/static/js/main.48a18b74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b4aa83c4b191d99aaed",
+    "url": "/reactdatabase/static/js/runtime-main.be68d893.js"
+  }
+]);
